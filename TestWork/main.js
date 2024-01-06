70d58640b875d5284d17c112516cf7074062ba6e
Yaroslav <129963434+yaroslavhroshev@users.noreply.github.com>
2024-01-07 01:17:53 +0200

    Main work has done

--- TestWork/main.js
+++ TestWork/main.js
@@ -1,10 +1,33 @@
 import { menuNavigationFunction } from './src/js/menu';
 import { customers, customersRenderFn } from './src/js/customers';
+import 'paginationjs';
 
 const menu = document.querySelector('.js-menu');
 menu.addEventListener('click', menuNavigationFunction);
 
 const customersTable = document.querySelector('.customers__list-wrapper > table');
 
-customersTable.insertAdjacentHTML('beforeend', customersRenderFn(customers).join(''));
-console.log(customersTable);
+$(document).ready(function () {
+  $('#pagination-container').pagination({
+    dataSource: [...customers],
+    pageSize: 8,
+    pageRange: 1,
+    className: 'paginationjs',
+    showPrev: true,
+    showNext: true,
+    callback: function (data, pagination) {
+      const html = customersRenderFn(data);
+      $('#data-container').html(function () {
+        html.unshift(`<tr class="customers__customers-table-header--border">
+        <th>Customer Name</th>
+        <th>Company</th>
+        <th>Phone Number</th>
+        <th>Email</th>
+        <th>Country</th>
+        <th>Status</th>
+      </tr>`);
+        return html;
+      });
+    },
+  });
+});
